55903ae38d5c245ec0864eb49a6ce65652880ae8
Richard Fenix <richard.fenix@gmail.com>
2018-02-23 15:23:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function theBeatlesPlay(musicians, instruments) {
   var beatlesPlay = []; 
   for (let i=0; i<musicians.length; i++) {
-    beatlesPlay.push(`${musicians[i]} plays ${guitar[i]}`);
+    beatlesPlay.push(`${musicians[i]} plays ${instruments[i]}`);
   }
+  console.log(beatlesPlay);
   return beatlesPlay;
 }
 
 var musicians = ['John Lenon', 'Ringo Star'];
-var instruments
+var instruments = ['guitar', 'drum'];
+
+theBeatlesPlay(musicians, instruments);
